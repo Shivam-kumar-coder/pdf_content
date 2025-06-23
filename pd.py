aa2da8667244121196933136d885f2469a85c64b
coder-shivam <innocentboy9472@gmail.com>
2025-06-23 12:33:58 +0530

    Create pd.py

---
+++ pd.py
@@ -0,0 +1,16 @@
+import re
+import streamlit as st
+from PyPDF2 import PdfReader
+
+# Function to extract text from PDF
+def extract_text_from_pdf(pdf_file):
+    pdf_reader = PdfReader(pdf_file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+        st.write(text)
+st.title("ATS Resume Parser")
+
+uploaded_file = st.file_uploader("Upload a Resume PDF", type="pdf")
+if uploaded_file:
+    extract_text_from_pdf(uploaded_file)
